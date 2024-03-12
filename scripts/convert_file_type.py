707bffaa33fb03fc66d6e52d62df0d4675394f07
Agent421 <eakoledoye@gmail.com>
2024-03-12 22:39:51 +0000

    gonna cook this shit in my own image

--- scripts/convert_file_type.py
+++
@@ -1,8 +0,0 @@
-import os
-from pydub import AudioSegment
-
-path = input("File: ")
-outpath = input("Output: ")
-
-audio = AudioSegment.from_file(path)
-audio.export(outpath)
--- scripts/download_excel.sh
+++
@@ -1,1 +0,0 @@
-wget -O sounds.xlsx https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=1uJjUwUxDAL1L9qc9qfmFBVhvyAv0cYEGIqtXSToFHbc&exportFormat=xlsx
