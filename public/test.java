051b7d6b8b76f9c9769336fded58133de0784bb3
gojodev <gojodev@users.noreply.github.com>
2024-05-25 23:07:37 +0100

    backup

---
+++ public/test.java
@@ -0,0 +1,6 @@
+writer.write(player1.getName());
+writer.write("\n"+ player2.getName());
+writer.write("\n"+ (int) ballSpeed);
+writer.write("\n"+ (int) ballSpeedIncrease);
+writer.write("\n"+ scoreLimit);
+writer.write("\n"+ (int) PLAYER_WIDTH);
