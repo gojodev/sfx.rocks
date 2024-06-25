d42bbd9516f8caccfd1f2380cdaf2ad6ab6abb1a
Agent421 <eakoledoye@users.noreply.github.com>
2024-06-24 22:16:26 -0400

    added the image url and the sound url for the sounds.json, updated sounds.json and category.json, ready to move back to front end development

--- public/main_test.js
+++ public/main_test.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import puppeteer from 'puppeteer';
 import fs from 'fs';
+import { json } from "express";
+import { join } from "path";
 
 //  sounds.json has more items than cateogory.json
 
@@ -18,36 +20,9 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const storage = getStorage();
-const categoryRef = ref(storage, 'category.json');
-const benImg = ref(storage, 'images/ben.webp');
-const imgFolder = benImg.parent;
-
-// will update the URLS to images (in the JSON array) from database
-async function updateURLS() {
-    let data = JSON.parse(fs.readFileSync('sounds.json', 'utf8'));
-    let length = data.length;
-    let current;
-    for (let i = 0; i < length; i++) {
-        current = data[i];
-        const currentRef = ref(storage, `images/${current.id}.webp`);
-        data[i].url = await getDownloadURL(currentRef);
-
-        console.log(i + 1, " / ", length);
-    }
-
-    // console.log('\nDONE');
-
-    data = JSON.stringify(data);
-
-    fs.writeFile('sounds.json', data, (err) => {
-        if (err) throw err;
-    })
-}
-
-updateURLS();
 
 // ! for text only
-async function getRef_text(refItem) {
+async function getRef(refItem) {
     const url = await getDownloadURL(refItem);
     const response = await fetch(url, { mode: 'cors' });
     let data = await response.text();
@@ -56,8 +31,72 @@ async function getRef_text(refItem) {
 }
 
 // ! get the links from images
+const benImg = ref(storage, 'images/ben.webp');
 const url = await getDownloadURL(benImg);
 
+// will update the URLS to images (in the JSON array) from database
+async function updateURLS() {
+    let data = JSON.parse(fs.readFileSync('sounds.json', 'utf8'));
+    let length = data.length;
+    let current;
+    for (let i = 0; i < length; i++) {
+        current = data[i];
+        data[i].category = current.category.charAt(0).toUpperCase() + current.category.slice(1);
+
+        const imgRef = ref(storage, `images/${current.id}.webp`);
+        const soundRef = ref(storage, `images/${current.id}.webp`);
+
+        data[i].img_url = await getDownloadURL(imgRef);
+        data[i].sound_url = await getDownloadURL(soundRef);
+
+        console.log(i + 1, " / ", length);
+    }
+
+    data = JSON.stringify(data);
+
+    // todo upload to firebase instead of manually updating uploading it
+    fs.writeFile('sounds.json', data, (err) => {
+        if (err) throw err;
+    })
+}
+
+// updateURLS();
+
+async function updateCategories() {
+    const soundsRef = ref(storage, 'sounds.json');
+    const catPromise = Promise.resolve(getRef(soundsRef));
+    catPromise.then((catPromise) => {
+        let categories = catPromise;
+        let length = categories.length;
+
+        var cats_arr = [];
+        var sorted = [];
+        var current;
+        for (let i = 0; i < length; i++) {
+            current = categories[i].category;
+            if (!cats_arr.includes(current)) {
+                cats_arr.push(current);
+                sorted.push([]);
+            }
+            let index = cats_arr.indexOf(current);
+            sorted[index].push(categories[i]);
+        }
+
+        let cats_json = {};
+        for (let i = 0; i < cats_arr.length; i++) {
+            cats_json[`${cats_arr[i]}`] = sorted[i];
+        }
+
+        cats_json = JSON.stringify(cats_json);
+
+        fs.writeFile('category.json', cats_json, (err) => {
+            if (err) throw err;
+        })
+
+    });
+}
+
+updateCategories();
 
 // todo: add select new image option
 async function imgScrape(queries) {
